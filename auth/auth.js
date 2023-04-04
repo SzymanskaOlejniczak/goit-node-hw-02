79d35cb74f87ebf09cd5e9b3379f55696f796189
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-04 12:27:24 +0200

    Create auth.js

---
+++ auth/auth.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const  {getUserByEmail}  = require("../conrollers/users");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const auth = async(req, res, next) => {
+  // pobieramy token z nagłówka
+  const token = req.headers.authorization;
+
+  // jeśli nie ma tokenu to nie puszamy dalej - mowimy ze brak tokenu
+  if (!token) {
+    return res.status(401).send("No token provided");
+  }
+
+  // jesli token jest to go weryfikujemy
+  try {
+    jwt.verify(token, jwtSecret);
+    req.user = jwt.decode(token);
+    // jesli wszystko poszło okay to wywołujemy next function
+    try {
+      const user = await getUserByEmail(req.user.email);
+      if (!user) {
+        return res.status(401).json({ message: "Not authorized" });
+      }
+    } catch (error) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+   
+    next();
+  } catch {
+    // jesli cos nie tak to odmawiamy dostepu
+    return res.status(401).send("Access denied");
+  }
+};
+
+module.exports = auth;
