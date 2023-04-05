42e7f1a0a34250ea49f4a87b8244ede98e0ef60f
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-05 19:56:07 +0200

    Update issueToken.js

--- auth/issueToken.js
+++ auth/issueToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const jwtSecret = process.env.JWT_SECRET;
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 const issueToken = (user) => {
   // ustalamy nasz payload (informacje ktore dodatkowo checmy zawrzec w tokenie)
@@ -10,7 +10,7 @@ const issueToken = (user) => {
   };
 
   // tworzymy nasz token (podpisujemy)
-  const token = jwt.sign(payload, jwtSecret);
+  const token = jwt.sign(payload, jwtSecretKey);
 
   // zwracamy token
   return token;
