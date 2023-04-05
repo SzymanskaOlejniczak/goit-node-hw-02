66f06774a2651bdc0daa260b6714957b74b642a1
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-05 20:06:50 +0200

    Update users.js

--- conrollers/users.js
+++ conrollers/users.js
@@ -1,18 +1,15 @@
-
 const { User, hashPassword } = require("../models/users");
 
 const createUser = async (body) => {
-  const {email, password}=body;
+  const { email, password } = body;
   // haszujemy hasło
   const hashedPassword = hashPassword(password);
-    // tworzymy usera z zahaszowanym hasłem
-    const newUser = await User.create({ email, password: hashedPassword }); 
-    return newUser;
-
+  // tworzymy usera z zahaszowanym hasłem
+  const newUser = await User.create({ email, password: hashedPassword });
+  return newUser;
 };
 const getUserByEmail = async (email) => {
   return await User.findOne({ email });
-  
 };
 const getAllUsers = async () => {
   const users = await User.find();
@@ -23,8 +20,6 @@ const getUserById = async (_id) => {
   const user = await User.findOne({ _id });
   return user;
 };
-
-
 
 const deleteUser = async (_id) => {
   try {
@@ -37,13 +32,21 @@ const deleteUser = async (_id) => {
 // updatujemy subscription usera
 
 const updateSubscription = async (email, body) => {
-const {subscription}=body;
-  const user = await User.findOneAndUpdate({ email }, { subscription }, { new: true });
+  const { subscription } = body;
+  const user = await User.findOneAndUpdate(
+    { email },
+    { subscription },
+    { new: true }
+  );
   return user;
 };
 //logout usera
 const logout = async (token) => {
-  const user = await User.findOneAndUpdate({ token }, { token: null }, { new: true });
+  const user = await User.findOneAndUpdate(
+    { token },
+    { token: null },
+    { new: true }
+  );
   return user;
 };
 
