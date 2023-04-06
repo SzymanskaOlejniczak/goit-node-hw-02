29db779820c412f85830eafc49e89b1bf8507250
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-06 19:43:12 +0200

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -4,19 +4,18 @@ const { getUserByEmail } = require("../conrollers/users");
 const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 const auth = async (req, res, next) => {
-  // pobieramy token z nagłówka
+  // get the token from the header
   const token = req.headers.authorization;
 
-  // jeśli nie ma tokenu to nie puszamy dalej - mowimy ze brak tokenu
+  // if there is no token, we do not continue - we say that there is no token
   if (!token) {
     return res.status(401).send("No token provided");
   }
-
-  // jesli token jest to go weryfikujemy
+ // if the token exists, we verify it
   try {
     jwt.verify(token, jwtSecretKey);
     req.user = jwt.decode(token);
-    // jesli wszystko poszło okay to wywołujemy next function
+   // if everything went okay, we call next function
     try {
       const user = await getUserByEmail(req.user.email);
       if (!user) {
@@ -28,7 +27,7 @@ const auth = async (req, res, next) => {
 
     next();
   } catch {
-    // jesli cos nie tak to odmawiamy dostepu
+   // if something is wrong, we deny access
     return res.status(401).send("Access denied");
   }
 };
