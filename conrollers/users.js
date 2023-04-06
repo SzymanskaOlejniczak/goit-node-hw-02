2ec3d1c54a20e390aa08fd130d7038b0b84aba2c
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-06 19:48:03 +0200

    Update users.js

--- conrollers/users.js
+++ conrollers/users.js
@@ -2,9 +2,9 @@ const { User, hashPassword } = require("../models/users");
 
 const createUser = async (body) => {
   const { email, password } = body;
-  // haszujemy hasło
+  // hash the password
   const hashedPassword = hashPassword(password);
-  // tworzymy usera z zahaszowanym hasłem
+// create a user with a hashed password
   const newUser = await User.create({ email, password: hashedPassword });
   return newUser;
 };
@@ -29,7 +29,7 @@ const deleteUser = async (_id) => {
   }
 };
 
-// updatujemy subscription usera
+// we update the subscription user
 
 const updateSubscription = async (email, body) => {
   const { subscription } = body;
@@ -40,7 +40,7 @@ const updateSubscription = async (email, body) => {
   );
   return user;
 };
-//logout usera
+//logout user
 const logout = async (token) => {
   const user = await User.findOneAndUpdate(
     { token },
