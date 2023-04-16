cef395f213c8198255fe89735b4418147e82e433
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-16 22:39:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const app = require('./app');
+const app = require("./app");
+// helper to handle folders
 const createFolderIfNotExist=require("./helpers/helpers");
 
-app.listen(8000, () => {
+app.listen(3000, () => {
   createFolderIfNotExist("./tmp");
   createFolderIfNotExist("./public");
   createFolderIfNotExist("./public/avatars");
-  console.log("Server running. Use our API on port: 8000");
+
+  console.log("Server running. Use our API on port: 3000");
 })
