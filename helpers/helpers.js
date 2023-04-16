14f54bf7a801391aa54eba941e5b102e0f139b54
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-16 22:38:43 +0200

    Update helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,12 +1,12 @@
 const fs = require("fs/promises");
-
+// check if our folder exists
 const isExist = (path) => {
   return fs
     .access(path)
     .then(() => true)
     .catch(() => false);
 };
-
+//if it doesn't exist, we create it
 const createFolderIfNotExist = async (path) => {
   if (!(await isExist(path))) {
     await fs.mkdir(path);
