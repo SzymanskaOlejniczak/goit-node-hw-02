afc5edd60415e459b87e36fde70057ea64318f94
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-18 23:40:32 +0200

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -6,14 +6,14 @@ const jwtSecretKey = process.env.JWT_SECRET_KEY;
 const auth = async (req, res, next) => {
   // get the token from the header
   const token = req.header("Authorization");
-  //console.log(token);
+  console.log(token);
   // if there is no token, we do not continue - we say that there is no token
   if (!token) {
     return res.status(401).send("No token provided");
   }
   // if the token exists, we verify it
   try {
-    //req.user = jwt.decode(token);
+    req.user = jwt.decode(token);
     jwt.verify(token, jwtSecretKey);
     
     // if everything went okay, we call next function
