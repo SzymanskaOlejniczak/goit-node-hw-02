f52c51f63bab9a73d6a3c2b1ad7beb23e37a206e
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-04-24 11:14:42 +0200

    Update loginHandler.js

--- auth/loginHandler.js
+++ auth/loginHandler.js
@@ -1,29 +1,22 @@
 const bcrypt = require("bcrypt");
-
 const { getUserByEmail } = require("../conrollers/users");
 const issueToken = require("./issueToken");
 
-const loginHandler = async (email, incomingPassword) => {
- // get the user
-  const user = await getUserByEmail(email);
+const loginHandler = async (email, password) => {
+	const user = await getUserByEmail(email);
+	if (!user) {
+		throw new Error('Email or password is wrong');
+	}
 
-  // if there is no user, return information
-  if (!user) {
-    throw { code: 404, msg: "User not found!!!" };
+  if (!user.verify) {
+    throw new Error("Email not verified");
   }
 
- // get our user's password
-  const userPassword = user.password;
-
-// compare passwords (incoming and user's)
-  const result = bcrypt.compareSync(incomingPassword, userPassword);
-
-// we return the token
-  if (result) {
-    return issueToken(user);
-  } else {
-    throw { code: 401, msg: "Invalid credentials" };
-  }
+  const result = await bcrypt.compare(password, user.password);
+	if (result) {
+		return issueToken(user);
+	} else {
+		throw new Error('Email or password is wrong');
+	}
 };
-
 module.exports = loginHandler;
